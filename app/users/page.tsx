4abcaf611207d1ac3529a2831b086029c414cf1e
gagandeep kaur <gagandeepkaur15@academic.rrc.ca>
2026-05-12 13:25:43 -0500

    added the local date option.

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -5,12 +5,12 @@ interface User {
 }
 
 export const UsersPage = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users',
-                          {next:{revalidate: 10}});
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', {cache: 'no-store'})
   const users: User[] = await res.json();
   return (
     <>
     <h1>Users</h1>
+    <p>{new Date().toLocaleDateString()}</p>
     <ul>
       {users.map(user => <li key={user.id}>{user.name}</li>)}
     </ul>
