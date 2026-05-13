405e28e98545df034bbf05b6e4f4c2d49e427d8d
gagandeep kaur <gagandeepkaur15@academic.rrc.ca>
2026-05-13 09:43:07 -0500

    added some basic padding, text color, background color and margins using tailwind.

--- app/users/components/productCard.tsx
+++ app/users/components/productCard.tsx
@@ -3,7 +3,7 @@ import AddToCart from './AddToCart';
 
 export const ProductCard = () => {
   return (
-    <div>
+    <div className='p-5 bg-sky-400 text-white test-xl hover:bg-sky-500'>
       <AddToCart />
     </div>
   
